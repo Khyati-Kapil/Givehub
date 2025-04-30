17030f12db75833d178fbe553ad28cc81b123897
Khyati <khyatekapil@gmail.com>
2025-04-30 23:31:40 +0530

    message

--- src/Components/Firebase.js
+++ src/Components/Firebase.js
@@ -7,15 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSH_joex7At62myusnU6axVR_JCizkXsI",
-  authDomain: "givehub-c3131.firebaseapp.com",
-  projectId: "givehub-c3131",
-  storageBucket: "givehub-c3131.firebasestorage.app",
-  messagingSenderId: "655361655407",
-  appId: "1:655361655407:web:01e888442a9d831187c39a",
-  measurementId: "G-JCWVQNH599"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY, //Vite uses import.meta.env to access environment variables instead of process.env
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);  
